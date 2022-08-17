27d42b03085e388b31954b691e98d1f46f890dfe
Renan Bruno Moura <64485870+RenanMour4@users.noreply.github.com>
2022-08-17 10:32:58 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,15 @@
+<?php
+    $host = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "projeto";
+
+    $conexao = new MySQLi("$host","$usuario","$senha","$banco");
+
+    if($conexao->connect_error){
+        echo "conexao_erro";
+    }
+    else{
+     //echo "conexao_ok";
+    }
+?>
